0e1a984c086e98e52a8568e476f270000ad1e000
Dhina17 <dhinalogu@gmail.com>
2024-05-25 11:24:54 +0530

    LMOClock: Init project
    Change-Id: Ide0341f43b1fbc67c3f962b5ffc311beb43e5b1d

---
+++ app/build.gradle.kts
@@ -0,0 +1,62 @@
+import java.io.FileInputStream
+import java.util.Properties
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+val keystorePropertiesFile = rootProject.file("keystore.properties")
+val keystoreProperties = Properties()
+keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+
+android {
+    namespace = "com.libremobileos.clock"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.libremobileos.clock"
+        minSdk = 34
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    signingConfigs {
+        create("config") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+}
+
+dependencies {
+
+    // System libs
+    compileOnly(files("libs/framework.jar"))
+    implementation(files("libs/SystemUISharedLib.jar"))
+    implementation(files("libs/SystemUI-statsd.jar"))
+    implementation(project(":SystemUIAnimationLib"))
+    implementation(project(":SystemUICustomizationLib"))
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+}
