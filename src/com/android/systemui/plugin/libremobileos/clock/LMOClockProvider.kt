173c1413052d14e59b61c4df90863856293f3778
Dhina17 <dhinalogu@gmail.com>
2024-05-25 11:26:08 +0530

    Trying something
    Change-Id: I94e61d3fef1f19c565b657b69699a2b8bf43a0d7

--- src/com/android/systemui/plugin/libremobileos/clock/LMOClockProvider.kt
+++ src/com/android/systemui/plugin/libremobileos/clock/LMOClockProvider.kt
@@ -5,25 +5,64 @@
 
 package com.android.systemui.plugin.libremobileos.clock
 
+import android.content.Context
 import android.graphics.drawable.Drawable
+import android.util.Log
 import com.android.systemui.plugins.annotations.ProvidesInterface
+import com.android.systemui.plugins.annotations.Requires
 import com.android.systemui.plugins.clocks.ClockController
 import com.android.systemui.plugins.clocks.ClockMetadata
 import com.android.systemui.plugins.clocks.ClockProviderPlugin
 import com.android.systemui.plugins.clocks.ClockSettings
-import com.android.systemui.shared.clocks.ClockId
 
+/** Identifies a clock design */
+typealias ClockId = String
+
+private val TAG = "dhina17test"
+const val LMO_CLOCK_ID = "LMOClock"
+
+@Requires(target = ClockProviderPlugin::class, version = ClockProviderPlugin.VERSION)
 @ProvidesInterface(action = ClockProviderPlugin.ACTION, version = ClockProviderPlugin.VERSION)
 class LMOClockProvider : ClockProviderPlugin {
+    private val hasStepClockAnimation: Boolean = false
+    private val migratedClocks: Boolean = false
+
+    private lateinit var pluginCtx: Context
+    private lateinit var sysuiCtx: Context
+
+    private val metadata = mutableListOf<ClockMetadata>()
+
+    override fun onCreate(sysuiCtx: Context, pluginCtx: Context) {
+        this.sysuiCtx = sysuiCtx
+        this.pluginCtx = pluginCtx
+        Log.e(TAG, "onCreate")
+        metadata.add(ClockMetadata(LMO_CLOCK_ID))
+    }
+
+    override fun onDestroy() {
+        Log.i(TAG, "onDestroy()")
+    }
+
     override fun createClock(settings: ClockSettings): ClockController {
-        TODO("Not yet implemented")
+        if (settings.clockId == null) {
+            throw IllegalArgumentException("No clockId specified")
+        }
+        Log.e(TAG, "createClock")
+        return LMOClockController(
+                pluginCtx,
+                settings,
+                hasStepClockAnimation,
+                migratedClocks,
+        )
     }
 
     override fun getClockThumbnail(id: ClockId): Drawable? {
-        TODO("Not yet implemented")
+        Log.e(TAG, "getClockThumbnail")
+        return null
     }
 
     override fun getClocks(): List<ClockMetadata> {
-        TODO("Not yet implemented")
+        Log.e(TAG, "getClocks()")
+        return metadata
     }
 }
